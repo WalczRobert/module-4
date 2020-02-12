dffe2d350c274f07acf438cb60c549d3df536ab2
rewalczak <rewalczak@gmail.com>
2020-02-11 19:46:04 -0600

    Add files via upload

---
+++ calculate_price.py
@@ -0,0 +1,40 @@
+"""Robert Walczak
+You are online shopping at SuperAwesomeCouponDealsAndStuff.com. They offer two types of discounts. The first is cash-off
+coupons, that you earn by shoppings. You may apply one $5 or $10 cash off per order. The second is percent discount
+coupons for 10%, 15%, or 20% off. If you have cash-off coupons, those must be applied first, then apply the percent
+discount coupons on the pre-tax amount. Then you add tax at 6% and shipping according to these guidelines:
+
+"""
+def calculate_price(price, cash_coupon, percent_coupon):
+    Total_Cash = price - cash_coupon
+    Total_Coupon = Total_Cash * (1-(percent_coupon/100))
+    if Total_Coupon <0:
+        Total_Coupon =0
+    print('Discount price is: ' + str(round(Total_Coupon,2)))
+    Total_Tax = Total_Coupon * 1+(0.06)
+    print('Your total with tax is : '+ str(round(Total_Tax,2)))
+    calculate_shipping(Total_Coupon, Total_Tax)
+    return(Total_Coupon)
+
+def calculate_shipping(Total_Coupon, Total_Tax):
+        if Total_Coupon < 10:
+            shipping = 5.95
+        elif 10 < Total_Coupon < 30:
+            shipping = 7.95
+        elif 30 < Total_Coupon < 50:
+            shipping = 11.95
+        else:
+            shipping = 0
+        grand_total = shipping + Total_Tax
+        print('Shipping charges are: '+ str(shipping))
+        print('our Total with shipping is: ' + str(round(grand_total,2)))
+
+
+
+
+if __name__ == '__main__':
+    price = int(input('What is your total?'))
+    cash_coupon = int(input('Do you have a $5 or $10  coupon? if yes which do you have?'))
+    percent_coupon = int(input("Do you have a 10%, 15% or 20% off coupon, if yes which do you have?"))
+    calculate_price(price, cash_coupon, percent_coupon)
+
---
+++ test_calculate_price.py
@@ -0,0 +1,19 @@
+'''Robert Walczak'''
+import unittest
+import unittest.mock as mock
+import calculate_price
+
+class FunctionTest(unittest.TestCase):
+    def test_price_under_ten(self):
+        under_ten = 7.0
+        # price under ten, 5 cash, 10%
+        self.assertEqual(calculate_price.calculate_price(under_ten, 5.0, 10.0), 1.80)
+        # price under ten, 5 cash, 15%
+        self.assertEqual(calculate_price.calculate_price(under_ten, 5.0, 15.0), 1.70)
+        # price under ten, 5 cash, 20%
+        self.assertEqual(calculate_price.calculate_price(under_ten, 5.0, 20.0), 1.60)
+         # price under ten, 10 cash, 10%
+        self.assertEqual(calculate_price.calculate_price(under_ten, 10.0, 10.0), -2.70)
+
+if __name__ == '__main__':
+    unittest.main()
